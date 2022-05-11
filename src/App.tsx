92a03124d314363fa2ae299c4dc0fdb060df7d51
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2022-05-11 13:45:22 -0700

    Configure authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useAuth } from 'react-oidc-context';
 
-function App() {
+export default function App() {
+
+  const auth = useAuth(); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { auth.isLoading ? <Loading /> : auth.isAuthenticated ? <Home /> : <Login /> }
     </div>
   );
 }
 
-export default App;
+function Loading() {
+  return (
+    <div className="card border-dark shadow">
+      <div className="card-body">
+        <div className="text-center text-primary">
+          <div className="spinner-border"></div>
+          <div className="h1 text-primary mt-3">Loading...</div>
+        </div>
+      </div>
+    </div>      
+  );
+}
+
+function Home() {
+  const auth = useAuth();
+
+  
+
+  const signout = () => {
+    auth.signoutRedirect();
+  }
+
+  return (
+    <>
+      <div className="card border-dark shadow mb-2">
+        <div className="card-body">
+          <h4 className="card-title" id="card-title">Trimble Identity Demo</h4>
+          <h5 className="card-subtitle mb-2 text-muted" id="card-subtitle">Welcome</h5>
+          <img src={auth.user?.profile?.picture} alt="Profile Picture" />
+          <p className="card-text">{auth.user?.profile?.given_name} {auth.user?.profile?.family_name}</p>
+          <p className="card-text">{auth.user?.profile?.email}</p>
+          <button className="btn btn-primary" onClick={signout}>Sign Out</button>
+        </div>
+      </div>      
+
+      <div className="card border-dark shadow mb-2">
+        <div className="card-body">
+          <h4 className="card-title" id="card-title">User Profile</h4>
+          <p className="card-text">{JSON.stringify(auth.user?.profile)}
+          </p>
+        </div>
+      </div>      
+
+    </>
+  )
+}
+
+function Login() {
+  const auth = useAuth();
+
+  const signin = () => {
+    console.log("Login");
+    auth.signinRedirect();
+  }
+
+  return(
+    <div className="card border-dark shadow">
+      <div className="card-body">
+        <h4 className="card-title" id="card-title">Trimble Identity Demo</h4>
+        <h5 className="card-subtitle mb-2 text-muted" id="card-subtitle">Sign In</h5>
+        <p className="card-text">Click the sign in button to login using Trimble Identity</p>
+        <button className="btn btn-primary" onClick={signin}>Sign In</button>
+      </div>
+    </div>      
+  )
+}
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
+
+const authProps: AuthProviderProps = {
+  authority: "https://stage.id.trimblecloud.com",
+  client_id: "96ef14e7-50ef-46cc-9f0c-bb2d8649b3df",
+  redirect_uri: "http://localhost:3000",
+  scope: "openid tdp-react-auth",
+}
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...authProps}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
